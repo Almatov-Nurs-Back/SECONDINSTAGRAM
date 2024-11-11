aff85f311aa6073f639a99324bd4e9baaa9deb62
Almatov-Nurs-Back <nalmatov6@gmail.com>
2024-11-11 17:15:17 +0600

    clear code

--- helpers/messages.py
+++ helpers/messages.py
@@ -17,6 +17,7 @@ def get_instagram_id(instagram_user_id=''):
     'user_id': instagram_user_id
   }
   response = requests.get(url=conversation_url, params=param).json()
+  print(f'data: {response}')
 
   # Проверяем, есть ли 'data' в ответе
   if 'data' in response and response['data']:
