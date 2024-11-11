198d3a8251ad2a5dd49be318c55bbb617b41c014
Almatov-Nurs-Back <nalmatov6@gmail.com>
2024-11-11 17:36:06 +0600

    clear code

--- helpers/openai_.py
+++ helpers/openai_.py
@@ -1,50 +1,34 @@
 import os
-from openai import OpenAI
+import openai
 from helpers.messages import get_messages
 
-client = OpenAI(
-  api_key=os.environ.get("OPENAI_API_KEY"),
-)
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not set in environment variables.")
+
+client = openai
 
 messages = dict()
 
 def generate_chatgpt_answer(sender_id='', prompt=''):
-  # ----- Code with ChatGPT -----
   if sender_id not in messages:
     messages[sender_id] = get_messages(sender_id)
-    # print(f'messages: {messages}')
-    pass
-  # -----------------------------
+
   print(messages[sender_id])
 
-  chat_completion = client.chat.completions.create(
+  chat_completion = client.ChatCompletion.create(
+    model="gpt-4o-mini",
     messages=[
       {
         "role": "system",
-        "content": "Ты ассистент отдыхного пансионата Иссык-Куле, юрточного городка \"Asman-Resort\". Ты консультируешь туристов, которые интересуятся отдыхм у нас.",
+        "content": "Ты ассистент отдыхного пансионата Иссык-Куле, юрточного городка \"Asman-Resort\". Ты консультируешь туристов, которые интересуятся отдыхом у нас.",
       },
-      # ----- Code with ChatGPT -----
       *messages[sender_id],
-      # -----------------------------
       {
         'role': 'user',
         'content': prompt
       }
     ],
-    model="gpt-4o-mini",
     max_tokens=1000,
   )
   return chat_completion.choices[0].message.content
-
-  # # ----- Code for testing -----
-  # if 'Привет' in prompt or 'привет' in prompt:
-  #   return 'Приветствую'
-  # elif 'Досвидание' in prompt or 'пока' in prompt:
-  #   return 'Всего доброго'
-  # elif 'кто ты' in prompt or 'Чем вы занимаетесь' in prompt or 'чем вы занимаетесь' in prompt:
-  #   return 'Я ai ассистент из "..." компании!'
-
-  # return prompt
-  # # ----------------------------
-
-# print(generate_chatgpt_answer(sender_id='1198676774465284', prompt='Чем вы занимаетесь?'))
