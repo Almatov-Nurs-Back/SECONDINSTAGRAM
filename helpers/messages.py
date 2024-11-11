215ede8ddf9a27f8597e711d722c3a540ce85501
Almatov-Nurs-Back <nalmatov6@gmail.com>
2024-11-11 16:47:41 +0600

    test openai api key

--- helpers/messages.py
+++ helpers/messages.py
@@ -1,8 +1,8 @@
-import requests, os
+import requests, os, asyncio
 from dotenv import load_dotenv
+import aiohttp
 
 load_dotenv()
-
 
 username = 'test51877'
 page_id = '364595800072411'
@@ -10,45 +10,55 @@ url = 'https://graph.facebook.com/v20.0/'
 page_access_token = os.getenv('INSTAGRAM_MARKER')
 
 def get_instagram_id(instagram_user_id=''):
- conversation_url = url + page_id + '/conversations'
- param = dict()
- param['platform'] = 'instagram'
- param['access_token'] = page_access_token
- param['user_id'] = instagram_user_id
- response = requests.get(url=conversation_url, params=param)
- response = response.json()
- return response['data'][0]['id']
+    conversation_url = url + page_id + '/conversations'
+    param = {
+        'platform': 'instagram',
+        'access_token': page_access_token,
+        'user_id': instagram_user_id
+    }
+    response = requests.get(url=conversation_url, params=param)
+    response = response.json()
+    return response['data'][0]['id']
 
-def get_messages_using_conversation_id(converstaion_id=''):
- message_url = url + converstaion_id
- message_param = dict()
- message_param['fields'] = 'messages'
- message_param['access_token'] = page_access_token
- # this will give us all the message_id in the conversation but not the messages directly
- response = requests.get(url=message_url, params=message_param)
- response = response.json()
- # so will get the details of the messages using their message_id
- message_ = []
- # we will traverse through all the message_id and get the data and store it inside a list
- for i in response['messages']['data']:
-  #print(i['id'])
- ################################ Get the message information #####################################
-  message_url_ = url + i['id']
-  message_get_param = dict()
-  # this field defines the data you want from the endpoint
-  message_get_param['fields'] = 'message,from'
-  message_get_param['access_token'] = page_access_token
-  message_get_response = requests.get(url =message_url_,params = message_get_param)
-  message_get_response = message_get_response.json()
-  try:
-   sender = message_get_response['from']['username']
-   message = message_get_response['message']
+async def fetch_message(session, message_id):
+    message_url = url + message_id
+    params = {
+        'fields': 'message,from',
+        'access_token': page_access_token
+    }
+    async with session.get(message_url, params=params) as response:
+        return await response.json()
 
-   response = {'role': 'user' if sender == username else 'assistant','content': message}
-   message_.append(response)
-  except:
-   pass
- return message_
+async def get_messages_using_conversation_id(conversation_id):
+    message_url = url + conversation_id
+    params = {
+        'fields': 'messages',
+        'access_token': page_access_token
+    }
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(message_url, params=params) as response:
+            data = await response.json()
+            message_ids = [msg['id'] for msg in data.get('messages', {}).get('data', [])]
+
+            tasks = [fetch_message(session, message_id) for message_id in message_ids]
+            messages_data = await asyncio.gather(*tasks)
+
+    messages = []
+    for message_data in messages_data:
+        try:
+            sender = message_data['from']['username']
+            message = message_data['message']
+            response = {
+                'role': 'user' if sender == username else 'assistant',
+                'content': message
+            }
+            messages.append(response)
+        except KeyError:
+            continue
+
+    return messages
 
 def get_messages(sender_id=''):
-  return get_messages_using_conversation_id(get_instagram_id(sender_id))
+    conversation_id = get_instagram_id(sender_id)
+    return asyncio.run(get_messages_using_conversation_id(conversation_id))
