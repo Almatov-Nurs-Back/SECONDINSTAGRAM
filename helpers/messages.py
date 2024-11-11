b08191d179b4b52081622b86527dfc4a3e89fa7e
Almatov-Nurs-Back <nalmatov6@gmail.com>
2024-11-11 17:02:34 +0600

    test openai api key

--- helpers/messages.py
+++ helpers/messages.py
@@ -10,21 +10,27 @@ url = 'https://graph.facebook.com/v20.0/'
 page_access_token = os.getenv('INSTAGRAM_MARKER')
 
 def get_instagram_id(instagram_user_id=''):
-    conversation_url = url + page_id + '/conversations'
-    param = {
-        'platform': 'instagram',
-        'access_token': page_access_token,
-        'user_id': instagram_user_id
-    }
-    response = requests.get(url=conversation_url, params=param)
-    response = response.json()
+  conversation_url = url + page_id + '/conversations'
+  param = {
+    'platform': 'instagram',
+    'access_token': page_access_token,
+    'user_id': instagram_user_id
+  }
+  response = requests.get(url=conversation_url, params=param).json()
+
+  # Проверяем, есть ли 'data' в ответе
+  if 'data' in response and response['data']:
     return response['data'][0]['id']
+  else:
+    # Возвращаем None или выбрасываем ошибку, если нет данных
+    print("No data found for the given instagram_user_id")
+    return None
 
 async def fetch_message(session, message_id):
     message_url = url + message_id
     params = {
-        'fields': 'message,from',
-        'access_token': page_access_token
+      'fields': 'message,from',
+      'access_token': page_access_token
     }
     async with session.get(message_url, params=params) as response:
         return await response.json()
@@ -50,8 +56,8 @@ async def get_messages_using_conversation_id(conversation_id):
       sender = message_data['from']['username']
       message = message_data['message']
       response = {
-          'role': 'user' if sender == username else 'assistant',
-          'content': message
+        'role': 'user' if sender == username else 'assistant',
+        'content': message
       }
       messages.append(response)
     except KeyError:
@@ -60,7 +66,9 @@ async def get_messages_using_conversation_id(conversation_id):
   return messages
 
 def get_messages(sender_id=''):
-    conversation_id = get_instagram_id(sender_id)
-    messages = asyncio.run(get_messages_using_conversation_id(conversation_id))
-    print(messages)
-    return messages
+  conversation_id = get_instagram_id(sender_id)
+  if conversation_id is None:
+    return []  # или лог ошибки
+  print(messages)
+  messages = asyncio.run(get_messages_using_conversation_id(conversation_id))
+  return messages
