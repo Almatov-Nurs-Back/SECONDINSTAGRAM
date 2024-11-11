d2f9a6b921a76a1fe1ce6981cd0c9e4d7540432c
Almatov-Nurs <nalmatov6@gmail.com>
2024-11-11 15:37:21 +0600

    test openai api key

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def webhook():
         if message.get('message'):
           text = message['message'].get('text')
           sender_id = message['sender']['id']
-          generated_answer = generate_chatgpt_answer(sender_id=sender_id, question=text)
+          generated_answer = generate_chatgpt_answer(sender_id=sender_id, prompt=text)
           print(generated_answer)
           send_message(sender_id, generated_answer)
   return 'OK', 200
--- helpers/openai_.py
+++ helpers/openai_.py
@@ -2,40 +2,50 @@ import os
 from openai import OpenAI
 from helpers.messages import get_messages
 
-# client = OpenAI(
-#   api_key=os.environ.get("OPENAI_API_KEY"),
-# )
+client = OpenAI(
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 messages = dict()
 
-def generate_chatgpt_answer(sender_id='', question=''):
-  # if sender_id not in messages:
-  #   messages[sender_id] = get_messages(sender_id)
-  #   print(messages)
-  #   pass
+def generate_chatgpt_answer(sender_id='', prompt=''):
+  # ----- Code with ChatGPT -----
+  if sender_id not in messages:
+    messages[sender_id] = get_messages(sender_id)
+    print(messages)
+    pass
+  print(sender_id)
+  # -----------------------------
 
-  # chat_completion = client.chat.completions.create(
-  #   messages=[
-  #     {
-  #       "role": "system",
-  #       "content": "Ты продавец машин, ты консультируешь клиентов, которые будут тебе писать.",
-  #     },
-  #     *messages[sender_id],
-  #     {
-  #       'role': 'user',
-  #       'content': question
-  #     }
-  #   ],
-  #   model="gpt-3.5-turbo",
-  # )
-  # return chat_completion.choices[0].message.content
-  if 'Привет' in question or 'привет' in question:
-    return 'Приветствую'
-  elif 'Досвидание' in question or 'пока' in question:
-    return 'Всего доброго'
-  elif 'кто ты' in question or 'Чем вы занимаетесь' in question or 'чем вы занимаетесь' in question:
-    return 'Я ai ассистент из "..." компании!'
-  else:
-    return question
+  chat_completion = client.chat.completions.create(
+    messages=[
+      {
+        "role": "system",
+        "content": "Ты ассистент отдыхного пансионата Иссык-Куле, юрточного городка \"Asman-Resort\". Ты консультируешь туристов, которые интересуятся отдыхм у нас.",
+      },
+      # ----- Code with ChatGPT -----
+      *messages[sender_id],
+      # -----------------------------
+      {
+        'role': 'user',
+        'content': prompt
+      }
+    ],
+    model="gpt-3.5-turbo-0125",
+    max_tokens=1000,
+  )
+  return chat_completion.choices[0].message.content
+  # -----------------------------
 
-# print(generate_chatgpt_answer('1198676774465284', 'Чем вы занимаетесь?'))
+  # # ----- Code for testing -----
+  # if 'Привет' in prompt or 'привет' in prompt:
+  #   return 'Приветствую'
+  # elif 'Досвидание' in prompt or 'пока' in prompt:
+  #   return 'Всего доброго'
+  # elif 'кто ты' in prompt or 'Чем вы занимаетесь' in prompt or 'чем вы занимаетесь' in prompt:
+  #   return 'Я ai ассистент из "..." компании!'
+
+  # return prompt
+  # # ----------------------------
+
+print(generate_chatgpt_answer(sender_id='1198676774465284', prompt='Чем вы занимаетесь?'))
