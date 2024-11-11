4f5f8e5e1a9cb9a75a39ed52f6f7eec1c025aa9f
Almatov-Nurs-Back <nalmatov6@gmail.com>
2024-11-11 16:56:47 +0600

    test openai api key

--- helpers/messages.py
+++ helpers/messages.py
@@ -30,35 +30,37 @@ async def fetch_message(session, message_id):
         return await response.json()
 
 async def get_messages_using_conversation_id(conversation_id):
-    message_url = url + conversation_id
-    params = {
-        'fields': 'messages',
-        'access_token': page_access_token
-    }
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(message_url, params=params) as response:
-            data = await response.json()
-            message_ids = [msg['id'] for msg in data.get('messages', {}).get('data', [])]
+  message_url = url + conversation_id
+  params = {
+    'fields': 'messages',
+    'access_token': page_access_token
+  }
 
-            tasks = [fetch_message(session, message_id) for message_id in message_ids]
-            messages_data = await asyncio.gather(*tasks)
+  async with aiohttp.ClientSession() as session:
+    async with session.get(message_url, params=params) as response:
+      data = await response.json()
+      message_ids = [msg['id'] for msg in data.get('messages', {}).get('data', [])]
 
-    messages = []
-    for message_data in messages_data:
-        try:
-            sender = message_data['from']['username']
-            message = message_data['message']
-            response = {
-                'role': 'user' if sender == username else 'assistant',
-                'content': message
-            }
-            messages.append(response)
-        except KeyError:
-            continue
+      tasks = [fetch_message(session, message_id) for message_id in message_ids]
+      messages_data = await asyncio.gather(*tasks)
 
-    return messages
+  messages = []
+  for message_data in messages_data:
+    try:
+      sender = message_data['from']['username']
+      message = message_data['message']
+      response = {
+          'role': 'user' if sender == username else 'assistant',
+          'content': message
+      }
+      messages.append(response)
+    except KeyError:
+      continue
+
+  return messages
 
 def get_messages(sender_id=''):
     conversation_id = get_instagram_id(sender_id)
-    return asyncio.run(get_messages_using_conversation_id(conversation_id))
+    messages = asyncio.run(get_messages_using_conversation_id(conversation_id))
+    print(messages)
+    return messages
